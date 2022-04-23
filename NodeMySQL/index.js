380bb093e40225c33f085ec8c5071649acad2018
PedroAraujo <pedro.nogueira@contazap.com>
2022-04-23 20:34:59 -0300

    desafio 2 do curso de Docker finalizado

---
+++ NodeMySQL/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const config = {
+    host: 'db-mysql',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+} 
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+connection.query(`create table people(id int not null auto_increment, name varchar(255), primary key(id))`)
+
+app.get('/', async (req, resp) => {
+    const sql = `INSERT INTO people(name) values ('Pedro')`
+    connection.query(sql)
+
+    const queryPeoples = `SELECT * FROM people`
+    connection.query(queryPeoples,  function (error, results, fields) {
+        const peoples = results.reduce((previousValue, currentValue) => 
+            previousValue + `${currentValue.name}<br>` , ` `)
+        
+            resp.send(`<h1>Full Cycle Rocks!</h1>
+            <h2>Parabéns, você está evoluindo!</h2>
+            
+            <h2>As pessoas presentes no banco são:<h2> 
+            <h3> ${peoples} </h3>
+        `)
+    })
+})
+
+app.listen(port, () => {
+    console.log('Running in port', port)
+})
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Docker_Nginx_Node_Mysql  
+
+### Como executar o projeto:  
+-> Para ver o resultado basta, após baixar o projeto, usar o seguinte comando no seu terminal:
+docker-compose up -d --build  
+
+### Descrição do projeto:  
+Esse projeto tem o objetivo de gerar um arquivo Docker Compose que
+suba um container de um NGINX que servirá como proxy Reverso, um container de uma aplicação Nodejs que será acessada pelo NGINX e um container com MySQL que será acessada pela aplicação Nodejs. Além disso para garantir que a aplicação Nodejs só suba após o MySQL ter subido usamos o Dockerize que é um script que vai garantir que o nosso Container Node só seja iniciado após o container Mysql estar no ar.  
+
+### Requisitos de software e bibliotecas:  
+-> Docker e Docker Compose. 
+
+### Links úteis:  
+-> Link do Dockerize: https://github.com/jwilder/dockerize     
+-> Proxy Reverso com NGINX: https://javascript.plainenglish.io/using-nginx-as-a-reverse-proxy-for-containerized-node-js-microservices-e7c4800f05a1  
